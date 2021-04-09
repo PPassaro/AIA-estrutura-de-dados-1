9994a57823a3a07d2f3dc73a3b664192314b0322
PPassaro <passaropriscilla@gmail.com>
2021-04-08 23:14:34 -0300

    Add numero 2 e 3

--- prova_a1.c
+++ prova_a1.c
@@ -20,6 +20,7 @@ void populate_students (Student *students) {
     scanf ("%li", &students[i].code);
 
     printf ("Diga o nome do Aluno %d: ", i+1);
+    // fflush(stdin)
     scanf("%s", students[i].name);
     // flush_in();
 
@@ -38,32 +39,60 @@ void reports (Student *students) {
   Student info_students[QTD_STUDENTS];
   int highest_grade;
   int i;
+  int average, highest_average, lowest_average, aux_highest, aux_lowest;
 
   populate_students(info_students);
 
   highest_grade = 0;
 
-  for (i = 1; i < QTD_STUDENTS; i++){
+  for (i = 0; i < QTD_STUDENTS; i++){
     if (info_students[i].grade1 > info_students[highest_grade].grade1) {
       highest_grade = i;
     }
   }  
+  
+  aux_highest = 0;
+  aux_lowest = 0;
 
-  for ( i = 1; i < QTD_STUDENTS; i++){
-   
+  for ( i = 0; i < QTD_STUDENTS; i++){
+    average = 0; 
+    average = (info_students[i].grade1 + info_students[i].grade2 + info_students[i].grade3) / 3;
+
+    if (average >= aux_highest) {
+      aux_highest = average;
+      highest_average = i;
+    } 
+    if (average < aux_lowest) {
+      aux_lowest = average;
+      lowest_average = i;
+    }
+
   }
 
-  printf ("Infos do estudante com maior nota da primeira prova:\n" );
-  printf ("\ncod\tNome\tNota 1\tNota 2\tNota 3");
-  printf ("\n%li\t%s\t%.2f\t%.2f\t%.2f\n", info_students[highest_grade].code, info_students[highest_grade].name, info_students[highest_grade].grade1, info_students[highest_grade].grade2, info_students[highest_grade].grade3);
+
+
+  printf ("\nInfos do estudante com maior nota da primeira prova:\n" );
+  printf ("\ncod\tNome\t\tNota 1\tNota 2\tNota 3");
+  printf ("\n%li\t%s\t\t%.2f\t%.2f\t%.2f\n", info_students[highest_grade].code, info_students[highest_grade].name, info_students[highest_grade].grade1, info_students[highest_grade].grade2, info_students[highest_grade].grade3);
+
+  printf ("\nAluno com MAIOR média geral:\n" );
+  printf ("\ncod\tNome\t\tNota 1\tNota 2\tNota 3\tMédia");
+  printf ("\n%li\t%s\t\t%.2f\t%.2f\t%.2f\t%.2lf\n", info_students[highest_average].code, info_students[highest_average].name, info_students[highest_average].grade1, info_students[highest_average].grade2, info_students[highest_average].grade3, (double)aux_highest);
+
+  printf ("\nAluno com MENOR média geral:\n" );
+  printf ("\ncod\tNome\t\tNota 1\tNota 2\tNota 3\tMédia");
+  printf ("\n%li\t%s\t\t%.2f\t%.2f\t%.2f\t%.2lf\n", info_students[lowest_average].code, info_students[lowest_average].name, info_students[lowest_average].grade1, info_students[lowest_average].grade2, info_students[lowest_average].grade3, (double)aux_lowest);
+
+
 }
 
 
 
 
+
 // -> OK (b) Encontrar e imprimir os dados do aluno com maior nota da primeira prova. 
-// (c) Encontrar e imprimir os dados do aluno com maior media geral.
-// (d) Encontrar e imprimir os dados do aluno com menor media geral.
+// -> OK (c) Encontrar e imprimir os dados do aluno com maior media geral.
+// -> OK (d) Encontrar e imprimir os dados do aluno com menor media geral.
 // (e) Para cada aluno diga se ele foi aprovado ou reprovado, considerando o valor 6 para aprovação. Apresente esta
 // informação na forma de uma tabela.
 
