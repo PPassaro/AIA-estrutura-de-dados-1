8b17d035127b9b27281cb8330e44438300c64a39
PPassaro <passaropriscilla@gmail.com>
2021-04-17 15:14:07 -0300

    Finalizando questao 3, 4 e 5

--- original/prova_a1.c
+++ original/prova_a1.c
@@ -110,6 +110,7 @@ void populate_teams (Team *teams){
 }
 
 int tournment (Team *teams){
+
   populate_teams(teams);
 
   int i = 0;
@@ -126,22 +127,143 @@ int tournment (Team *teams){
       victory_team_index = i;
     }
   }    
-
   // printf("indice: %i\n", victory_team_index);
 
 return victory_team_index;
 }
 
+// Questão 3 //
 
+void max_min (int *v, int N, int *max, int *min) {
+  int i;
 
-int main () {
+  for(i = 0; i < N; i++){    
+    int currentValue = v[i];
+    
+    if (i == 0){
+      *max = currentValue;
+      *min = currentValue;
+    }
+
+    if(currentValue > *max){
+      *max = currentValue;
+    }
+
+    if (currentValue < *min){
+      *min = currentValue;
+    }
+  }
+}
+
+// Questão 4 //
+
+int bin_search (int *v, int begin, int end, int value) {
+  while (begin <= end) { 
+    int m = (begin + end)/2;
+    if (value == v[m]) return m;
+    if (value < v[m]) end = m - 1; 
+    else begin = m + 1;
+  }
+  return 1;
+
+}
+
+// Questão 5 //
+
+int mult( int x, int n ){
+  if( n == 1)
+    return x;
+  return x + mult(x, n-1);
+}
+
+void questao_1() {
+   // Questão 1
   Student info_students[QTD_STUDENTS];
   reports(info_students);
+}
 
+void questao_2() {
+  // Questão 2
   Team info_teams[QTD_TEAMS];
   tournment(info_teams);
+}
 
+void questao_3() {
+ // Questão 3
+  int N = 5;
 
+  int vetNumber[5] = {10, 20, 30, 40, 50};
+
+  int max = 0;
+  int min = 0;
+
+  max_min(vetNumber, N, &max, &min);
+
+  printf("Max value: %i\n", max);
+  printf("Min value: %i\n", min);
+}
+
+void questao_4() {
+  // Questão 4 
+  int vetNumber[5] = {10, 20, 30, 40, 50};
+  int index = bin_search(vetNumber, 0, 3, 100);
+  printf("Result: %i\n", index);
+}
+
+void questao_5() {
+  // Questão 5
+  int result = mult(3, 4);
+  printf ("Result: %i\n", result);
+}
+
+void menu () {
+  int valor_inse;
+
+    do {
+      printf("\nInsira o numero da questão que deseja rodar\n");
+      printf("1. Questão 1\n");
+      printf("2. Questão 2\n");
+      printf("3. Questão 3\n");
+      printf("4. Questão 4\n");
+      printf("5. Questão 5\n");
+      printf("0. Sair\n");
+
+      scanf("%d", &valor_inse);
+
+      switch(valor_inse) {
+        case 1:
+          questao_1();
+          break;
+
+        case 2:
+          questao_2();
+          break;
+
+        case 3:
+          questao_3();
+          break;
+
+        case 4:
+          questao_4();
+          break;
+        
+        case 5:
+          questao_5();
+          break;
+        
+        case 0:
+          printf("Saindo\n");
+          break;
+
+        default:
+          printf("Digite uma opcao valida\n");
+      }
+  } while(valor_inse != 0);
+}
+
+int main () {
+
+  menu();
 
   return 0;
 }
